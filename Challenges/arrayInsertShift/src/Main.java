fa59d9766b83fa93731dfa95540d6f33486252d3
QusaiTA <tashtoshqusai@gmail.com>
2021-09-29 17:11:15 +0300

    rowSum solved

--- Challenges/arrayInsertShift/src/Main.java
+++ Challenges/arrayInsertShift/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5,6,7};
+        int[] arr = {1, 2, 3, 4, 5,6,7,8};
         System.out.print(Arrays.toString(insertShiftArray(arr,7)));
     }
 
