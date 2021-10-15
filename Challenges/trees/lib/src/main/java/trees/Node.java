6c6ed12a8ab7d2eb4e5522603a094b8b6bdf6cd8
QusaiTA <tashtoshqusai@gmail.com>
2021-10-15 15:10:53 +0300

    solved

---
+++ Challenges/trees/lib/src/main/java/trees/Node.java
@@ -0,0 +1,47 @@
+package trees;
+
+public class Node<T> {
+
+    private T data;
+    private Node<T> leftNode;
+    private Node<T> rightNode;
+
+    public Node() {
+    }
+
+    public Node(T data){
+        this.data=data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(Node<T> leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public Node<T> getRightNode() {
+        return rightNode;
+    }
+
+    public void setRightNode(Node<T> rightNode) {
+        this.rightNode = rightNode;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "leftNode=" + leftNode +
+                ", rightNode=" + rightNode +
+                '}';
+    }
+}
