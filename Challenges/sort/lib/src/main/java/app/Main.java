c20f8020d549ada4b7373e57d060288210d32aec
QusaiTA <tashtoshqusai@gmail.com>
2021-10-31 23:15:56 +0200

    solved

---
+++ Challenges/sort/lib/src/main/java/app/Main.java
@@ -0,0 +1,22 @@
+package app;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+        Library library = new Library();
+
+
+        int [] arr = {8,4,23,42,16,15};
+        int [] arr2 = {20,18,12,8,5,-2};
+        int [] arr3 = {5,12,7,5,5,7};
+
+        System.out.println(Arrays.toString(library.insertionSort(arr)));
+        System.out.println(Arrays.toString(library.insertionSort(arr2)));
+        System.out.println(Arrays.toString(library.insertionSort(arr3)));
+
+    }
+}
