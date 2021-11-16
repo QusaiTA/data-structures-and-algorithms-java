cdbbc56a120cf02ee5ac0f19d68941a6cc8b8fe7
QusaiTA <tashtoshqusai@gmail.com>
2021-11-16 09:29:41 +0200

    some edit

--- Challenges/graph/lib/src/main/java/app/Main.java
+++ Challenges/graph/lib/src/main/java/app/Main.java
@@ -22,12 +22,7 @@ public class Main {
         myGraph.addEdge("mohammad","testBusinessTrip",1);
 //        myGraph.addEdge("qusai" , "loai" , 2);
 //        System.out.println(myGraph.BFV("loai"));
-//        Graph<String> myGraph = new Graph<>();
-//        System.out.println(myGraph.addNode("Hello"));
-//        System.out.println(myGraph.addNode("Helklo"));
-//        myGraph.addEdge("Hello" , "mor" , 1);
-//        myGraph.addEdge("Helklo" , "mor" , 2);
-//        System.out.println(myGraph.BFV("Hello"));
+//
         String[] arr = {"qusai","testBusinessTrip","mohammad"};
         String cost =  myGraph.businessTrip(arr);
         System.out.println(myGraph);
